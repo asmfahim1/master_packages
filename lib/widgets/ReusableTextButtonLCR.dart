0fde05b6e3073f0cc4735b415594848c931d666b
asmfahim1 <asmfahim1@gmail.com>
2022-05-17 16:38:15 +0600

    interface changed

--- lib/widgets/ReusableTextButtonLCR.dart
+++ lib/widgets/ReusableTextButtonLCR.dart
@@ -11,7 +11,7 @@ class ReusableTextButtonL extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 40,
+      height: 45,
       child: TextButton(
         style: TextButton.styleFrom(
           shape: RoundedRectangleBorder(
